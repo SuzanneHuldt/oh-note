26c80115dc1ff8d096e353260a03a5927745991d
Peter Johnstone <peter.wdj@gmail.com>
2017-12-05 16:22:31 +0000

    Add functionality to allow a note to be saved to a notebook

--- interfaceLayer.js
+++ interfaceLayer.js
@@ -1,9 +1,11 @@
 var notebook = new NoteList();
 
 var addButton = document.querySelector('#create-new-note');
+var noteContent = document.querySelector('#new-note-text');
+
 addButton.addEventListener('click', function(event) {
-  
-  note = new Note('Hello, world');
-  console.log(note.text);
+  note = new Note(noteContent.value);
+  notebook.save(note);
+  noteContent.value = '';
   event.preventDefault();
-})
+});
