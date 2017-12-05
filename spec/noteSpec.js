0a09bea2e21aa8bad03a5959687a879b596c7fb2
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-05 15:15:47 +0000

    Added date and name to note

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -1,10 +1,33 @@
   function testNoteHasTextContent() {
-    var note = new Note('hello');
+    var note = new Note('Greeting', 'hello');
     if(note.text!=="hello") {
-      throw new Error("Note text is not an empty string")
+      throw new Error("Note text is not a string")
     }
     else {
       console.log("testNoteHasTextContent has passed")
     }
   };
   testNoteHasTextContent();
+
+  function testNoteHasName() {
+    var newnote = new Note('Fred', 'Ben&Jerry');
+    if(newnote.name() !== "Fred") {
+      throw new Error ("Note text doesn't have a name or text")
+    }
+    else {
+      console.log("testNoteHasName has passed")
+    }
+  }
+  testNoteHasName();
+
+  function testNoteHasDate() {
+    var note1 = new Note('Fred', 'Ben&Jerry');
+    if(note1.date() == '') {
+      throw new Error ("Note text doesn't have a name or text")
+    }
+    else {
+      console.log("testNoteHasDate has passed")
+    }
+  }
+
+testNoteHasDate();
--- src/note.js
+++ src/note.js
@@ -1,11 +1,23 @@
 'use strict'
 
-function Note(string) {
+function Note(name, string) {
   this.text = string;
-
+  this._name = name;
+  this._date = new Date();
 };
 
 Note.prototype.updateText = function (newtext) {
   this.text = newtext;
   return this.text;
 };
+
+Note.prototype.name = function() {
+  return this._name;
+};
+
+Note.prototype.date = function() {
+  return this._date;
+};
+
+
+console.log(this._date);
