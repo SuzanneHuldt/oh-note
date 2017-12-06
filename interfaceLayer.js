b5dbc13237edd2b65682b1fce9b181a600db1b83
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-12-06 16:47:31 +0000

    Added text overflow to notes & refactored

--- interfaceLayer.js
+++ interfaceLayer.js
@@ -5,17 +5,28 @@ var noteText = document.querySelector('#new-note-text');
 var addButton = document.querySelector('#create-new-note');
 var allNotes = document.querySelector('.all-notes');
 
-addButton.addEventListener('click', function(event) {
-  var newNote = document.createElement('div');
-  var newNoteTitle = document.createElement('h1');
-  var newNoteText = document.createElement('p');
+function preventEmptyNote(){
   if (noteTitle.value == false) {
     noteTitle.value = 'try a title?'
+
   }
   if (noteText.value == false) {
     noteText.value = 'write something?'
   }
+}
+
+
+addButton.addEventListener('click', function(event) {
+  var newNote = document.createElement('div');
+  var newNoteTitle = document.createElement('h1');
+  var newNoteText = document.createElement('p');
+
+  preventEmptyNote();
+
   var note = new Note(noteTitle.value, noteText.value);
+
+
+
   notebook.save(note);
   noteText.value = '';
   noteTitle.value = '';
@@ -26,6 +37,10 @@ addButton.addEventListener('click', function(event) {
   newNote.appendChild(newNoteText);
   newNoteText.setAttribute('class', 'note-text');
   newNoteTitle.innerHTML = notebook._notes[notebook._notes.length - 1]._name;
-  newNoteText.innerHTML = notebook._notes[notebook._notes.length - 1].text.slice(0, 50) + '...';
+  if (notebook._notes[notebook._notes.length - 1].text.length > 50) {
+    newNoteText.innerHTML = notebook._notes[notebook._notes.length - 1].text.slice(0, 50) + '...';
+  } else {
+    newNoteText.innerHTML = notebook._notes[notebook._notes.length - 1].text.slice(0, 50);
+  };
   event.preventDefault();
 });
