57ef0da3f61b0a9b2046ed0e66c1553936a7f9b3
Peter Johnstone <peter.wdj@gmail.com>
2017-12-06 16:00:27 +0000

    Pushing latest version of code for Marie

--- interfaceLayer.js
+++ interfaceLayer.js
@@ -1,18 +1,31 @@
 var notebook = new NoteList();
 
 var noteTitle = document.querySelector('#new-note-title');
-var noteContent = document.querySelector('#new-note-text');
+var noteText = document.querySelector('#new-note-text');
 var addButton = document.querySelector('#create-new-note');
 var allNotes = document.querySelector('.all-notes');
 
 addButton.addEventListener('click', function(event) {
-  var div = document.createElement('div');
-  var note = new Note(noteTitle.value, noteContent.value);
+  var newNote = document.createElement('div');
+  var newNoteTitle = document.createElement('h1');
+  var newNoteText = document.createElement('p');
+  if (noteTitle.value == false) {
+    noteTitle.value = 'try a title?'
+  }
+  if (noteText.value == false) {
+    noteText.value = 'write something?'
+  }
+  var note = new Note(noteTitle.value, noteText.value);
   notebook.save(note);
-  noteContent.value = '';
+  noteText.value = '';
   noteTitle.value = '';
-  allNotes.appendChild(div);
-  div.setAttribute('class', 'note');
-  div.innerHTML = notebook._notes[notebook._notes.length - 1]._name + ' ' + notebook._notes[notebook._notes.length - 1].text;
+  allNotes.appendChild(newNote);
+  newNote.setAttribute('class', 'note');
+  newNote.appendChild(newNoteTitle);
+  newNoteTitle.setAttribute('class', 'note-title');
+  newNote.appendChild(newNoteText);
+  newNoteText.setAttribute('class', 'note-text');
+  newNoteTitle.innerHTML = notebook._notes[notebook._notes.length - 1]._name;
+  newNoteText.innerHTML = notebook._notes[notebook._notes.length - 1].text.slice(0, 20);
   event.preventDefault();
 });
