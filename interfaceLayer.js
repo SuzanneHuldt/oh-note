e24883a22b5e87e976f13c79d011006d4c24f327
gabrielabud <gabriela.budeanu@gmail.com>
2017-12-07 12:19:05 +0000

    Hide elements when note is clicked

--- interfaceLayer.js
+++ interfaceLayer.js
@@ -4,12 +4,10 @@ var noteTitle = document.querySelector('#new-note-title');
 var noteText = document.querySelector('#new-note-text');
 var addButton = document.querySelector('#create-new-note');
 var allNotes = document.querySelector('.all-notes');
-var singleNote = document.querySelector('.note');
 
 function preventEmptyNote(){
   if (noteTitle.value == false) {
     noteTitle.value = 'try a title?'
-
   }
   if (noteText.value == false) {
     noteText.value = 'write something?'
@@ -19,6 +17,8 @@ function preventEmptyNote(){
 function createNoteOnPage(div,title,text) {
   allNotes.insertBefore(div, allNotes.childNodes[0]);
   div.setAttribute('class', 'note');
+  div.setAttribute('href', `#${notebook._notes[notebook._notes.length-1]._id}`);
+  div.setAttribute('id', notebook._notes[notebook._notes.length-1]._id);
   div.appendChild(title);
   title.setAttribute('class', 'note-title');
   div.appendChild(text);
@@ -40,6 +40,17 @@ function addTextToNote(title,content) {
   };
 }
 
+function addNoteEventListener(allNotes) {
+  allNotes.addEventListener('click', function() {
+    document.querySelector('#overlay').classList.remove('light-overlay');
+    document.querySelector('#overlay').classList.add('dark-overlay');
+    document.querySelector('#title').setAttribute('style','color: white;');
+    document.querySelector('#new-note-form').setAttribute('style','display: none;');
+    document.querySelector('.note').setAttribute('style','display: none;');
+    // this.setAttribute('style','display: inline-block;')
+  });
+}
+
 function createNote() {
   addButton.addEventListener('click', function(event) {
     var newNote = document.createElement('div');
@@ -51,11 +62,9 @@ function createNote() {
     createNoteOnPage(newNote,newNoteTitle,newNoteText);
     addTextToNote(newNoteTitle,newNoteText);
     event.preventDefault();
+    var notes = document.querySelector('.note');
+    addNoteEventListener(notes);
   });
 }
 
 createNote();
-
-// singleNote.addEventListener('click', function() {
-//   console.log("what");
-// });
