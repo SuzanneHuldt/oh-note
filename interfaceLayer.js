1da042682fab6c0c5a0dadbe9122820455a704c6
Peter Johnstone <peter.wdj@gmail.com>
2017-12-08 11:27:43 +0000

    Kill global variables and move logic to fullScreenNotes.js helper

--- interfaceLayer.js
+++ interfaceLayer.js
@@ -1,45 +1,7 @@
 var notebook = new NoteList();
 
-var noteTitle = document.querySelector('#new-note-title');
-var noteText = document.querySelector('#new-note-text');
-var addButton = document.querySelector('#create-new-note');
-var allNotes = document.querySelector('.all-notes');
-
-function changeNotes(addr){
-  document.querySelector('#overlay').classList.remove('light-overlay');
-  document.querySelector('#overlay').classList.add('dark-overlay');
-  document.querySelector('#title').setAttribute('style','color: white;');
-  document.querySelector('#new-note-form').setAttribute('style','display: none;');
-
-  var noteNow = addr;
-  var notes = document.querySelector('.all-notes').children;
-  for (var index = 0; index < notes.length; index++) {
-    var childNote = notes[index];
-    if (childNote.id !== noteNow) {
-      childNote.style.display = 'none';
-    };
-  };
-}
-
-function hashChangeEventListener(){
-  window.addEventListener("hashchange", function() {
-    var addr = getNrFromUrl();
-    changeNotes(addr);
-  });
-}
-function addNoteEventListener(allNotes) {
-  allNotes.addEventListener('click', function() {
-    console.log(this.id);
-    document.location.href = `#${this.id}`,true;
-  });
-}
-
-function getNrFromUrl() {
-  return window.location.hash.split("#"[1]);
-};
-
 function createNote() {
-  addButton.addEventListener('click', function(event) {
+  findAddButton().addEventListener('click', function(event) {
     preventEmptyNote();
     createNoteAndResetForm();
     createNoteElement(findNoteContainer());
@@ -48,4 +10,5 @@ function createNote() {
   });
 }
 
+hashChangeEventListener();
 createNote();
