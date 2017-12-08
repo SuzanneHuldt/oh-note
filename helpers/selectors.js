dd09353cf8af99d9e7b7bc81d4fbfca38af17526
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-08 10:01:46 +0000

    refactor global variables with selectors

---
+++ helpers/selectors.js
@@ -0,0 +1,16 @@
+function findNoteTitle() {
+  return document.querySelector('#new-note-title');
+};
+
+function findNoteText() {
+  return document.querySelector('#new-note-text');
+};
+
+function findAddButton() {
+  return document.querySelector('#create-new-note');
+};
+
+function findAllNotes() {
+  return document.querySelector('.all-notes');
+};
+
