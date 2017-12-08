26e887fae241f2955e4456b6bae6e9894f01a13a
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-12-08 10:30:11 +0000

    adds preview function to Note

--- src/note.js
+++ src/note.js
@@ -23,3 +23,11 @@ Note.prototype.date = function() {
 Note.prototype.id = function() {
   return this._id;
 };
+
+Note.prototype.preview = function() {
+  if (this.text.length > 50) {
+    return this.text.slice(0, 47) + '...';
+  } else {
+    return this.text;
+  };
+};
